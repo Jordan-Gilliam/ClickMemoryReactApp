a1a2a01e948b96755a66d7956c1ca73a3eadb6f3
Jordan Gilliam <gilliamja.te@gmail.com>
2018-01-13 08:03:44 +0000

    app structure

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,21 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import FriendCard from "./components/FriendCard";
+import friends from "./friends.json";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+// Advanced .map(loop) method
+
+// const App = () => (
+//   const cards = 
+//   friends.map(function(friend) {
+//       return
+//       <FriendCard
+//       name={friend.name}
+//       image={friend.image}
+//       occupation={friend.occupation}
+//       location={friend.location}
+//       />;
+//     });
+  
+//   return 
+//   <Wrapper>
+//     <Title>Friends List</Title>
+//     {cards} 
+//   </Wrapper>
+// );
+
+
+const App = () => (
+  <Wrapper>
+    <Title>Friends List</Title>
+    <FriendCard
+      name={friends[0].name}
+      image={friends[0].image}
+      occupation={friends[0].occupation}
+      location={friends[0].location}
+    />
+    <FriendCard
+      name={friends[1].name}
+      image={friends[1].image}
+      occupation={friends[1].occupation}
+      location={friends[1].location}
+    />
+    <FriendCard
+      name={friends[2].name}
+      image={friends[2].image}
+      occupation={friends[2].occupation}
+      location={friends[2].location}
+    />
+  </Wrapper>
+);
 
 export default App;
